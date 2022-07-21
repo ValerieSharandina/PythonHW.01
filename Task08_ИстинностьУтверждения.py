6d6c442e085c1c38acb67247b9439a8eaaa710fa
Valerie Sharandina <103958156+ValerieSharandina@users.noreply.github.com>
2022-07-21 19:53:24 +0300

    Update Task08_ИстинностьУтверждения.py
    переписала с выводом таблицы

--- Task08_ИстинностьУтверждения.py
+++ Task08_ИстинностьУтверждения.py
@@ -2,9 +2,11 @@
  Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 '''
 
-print('Введи три числа через пробел:')
-x, y, z = map(int, input().split())
-if not (x or y or z) == (not x and not y and not z):
-    print('Утверждение истинно')
-else:
-    print('Получается что не получается')
+print ('\nX \t Y \t Z \t ¬(X ⋁ Y ⋁ Z) \t ¬X ⋀ ¬Y ⋀ ¬Z \t ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z \n')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            on_left = not(x or y or z)
+            on_right = not(x) and not(y) and not (z)
+            print(
+                f'{x} \t {y} \t {z} \t {on_left} \t {on_right} \t {on_left == on_right}')
